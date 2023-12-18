5b1f8edb4ff92faae2e80ad44c005b2ad970210a
pattarapon <honamiyue@gmail.com>
2023-12-18 08:54:17 +0700

    complete

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 int main(){	
+	double innitial, interestRate, pay;
 	cout << "Enter initial loan: ";
+	cin >> innitial;
 	cout << "Enter interest rate per year (%): ";
+	cin >> interestRate;
 	cout << "Enter amount you can pay per year: ";
+	cin >> pay;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -17,17 +21,31 @@ int main(){
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
+
+	double balance = innitial;
+	int year = 1;
+	while (balance > 0){
+		double interest = balance*(interestRate/100);
+		double total = interest + balance;
+		double payment = (pay < total) ? pay : total;
+        double newBalance = total - payment;
+      
+
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	cout << setw(13) << left << year; 
+	cout << setw(13) << left << balance;
+	cout << setw(13) << left << interest;
+	cout << setw(13) << left << total;
+	cout << setw(13) << left << payment;
+	cout << setw(13) << left << newBalance;
+	cout << "\n";
+
+	balance = newBalance;
+	year++;	
+	}
 	
 	return 0;
 }
